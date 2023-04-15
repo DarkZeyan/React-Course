5d79a21897438f13f7726ac56cfcb389c47143d3
DarkZeyan <esonic0234@hotmail.com>
2023-04-15 15:39:10 -0600

    Comparison of the first work done

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,14 @@
+
+// export function App(){
+//     return (<h1>Hola Mundo!!!</h1>);
+// }
+
+// export const hola ='123  '
+// export default App;
+
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>Hello World App</h1>
+  )
+}
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+
+//Los functional components siempre van capitalizados.
+import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <HelloWorldApp/>
+        <FirstApp/>
+    </React.StrictMode>
+);
