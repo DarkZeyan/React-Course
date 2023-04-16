b77201892ce14faf414dce225d0583fc25dabbd7
DarkZeyan <esonic0234@hotmail.com>
2023-04-15 19:59:54 -0600

    Applying styles to React components

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,10 +1,12 @@
 // Si este valor no se cambia debe de estar fuera dle functional component.
-const newMessage = {
-  message:'Hola Mundo',
-  title:'Jorge'
-};
 
-const getSaludo= (nombre) => `Hola ${nombre}`;
+
+// const newMessage = {
+//   message:'Hola Mundo',
+//   title:'Jorge'
+// };
+
+// const getSaludo= (nombre) => `Hola ${nombre}`;
 //Esto es util ya que react no necesitara reprocesarlo ya que esta fuera del componente.
 
 export const FirstApp = () => {
@@ -18,7 +20,8 @@ export const FirstApp = () => {
     {/* Si necesitamos enviar el objeto
     podemos usar la expresion JSON.stringify ya que no podemos imprimir el objeto directamente. 
     No pueden ser  funciones asincronas debido a que estas esperan objetos de tipo promesa*/}
-      <h1>{getSaludo('Valentina')}</h1>
+      {/* <h1>{getSaludo('Valentina')}</h1> */}
+      <h1>Jorge Bugarini</h1>
       {/* <code>{JSON.stringify(newMessage)}</code> */}
       <p>Primera aplicacion.</p>
       </>
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 //Los functional components siempre van capitalizados.
 import { HelloWorldApp } from './HelloWorldApp';
+//Aplicacion de estilos de css.
+import './style.css'
 import { FirstApp } from './FirstApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
