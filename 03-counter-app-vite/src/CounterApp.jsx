7d8c135f4e57e841f28cc76f9a6dab9931916114
DarkZeyan <esonic0234@hotmail.com>
2023-04-15 21:04:31 -0600

    Finished useState hook lesson

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,13 +1,28 @@
-import PropTypes from 'prop-types'
+//Los hooks se importan desde react. Siempre deben de usar el use.
+import {useState} from 'react'
+
+import PropTypes from 'prop-types';
+
+//El redibujo de la pagina se hace con el uso del hook de useState.
 
 const CounterApp = ({value}) =>{
+    //El uso de este hook es es la desestructuracion del retorno de la funcion useState.
+    const [counter,setCounter] = useState(value);
 
+    const handleAdd =() =>{
+        // console.log(event)
+
+        setCounter(counter+1);
+        // setCounter((c)=>c+1);
+    }
     return(
         <>
 
         <h1>CounterApp</h1>
-        <p>{value}</p>
+        <p> { counter } </p>
 
+
+        <button onClick={handleAdd}>+1</button>
         </>
     )
 
