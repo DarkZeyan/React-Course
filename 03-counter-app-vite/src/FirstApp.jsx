d39323ce6152d3925347e317746206f5f49c904a
DarkZeyan <esonic0234@hotmail.com>
2023-04-15 20:23:21 -0600

    Finished proptypes lesson

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,3 +1,6 @@
+import PropTypes from 'prop-types'
+
+
 // Si este valor no se cambia debe de estar fuera dle functional component.
 
 
@@ -12,10 +15,18 @@
 // Los props ya no se utilizan ya que 
 // ahora se utiliza la desestructuracion.
 // export const FirstApp = ({title = ' Hola soy Zeyan'}) => {
+
+
+
+
 export const FirstApp = ({title,subTitle}) => {
 
 
 // console.log(props);
+
+  // if(!title){
+  //   throw new Error('El title no existe');
+  // }
 
   return (
     <>
@@ -29,4 +40,12 @@ export const FirstApp = ({title,subTitle}) => {
       <p>{subTitle}</p>
       </>
   )
+}
+
+//Los proptypes nos ayuda a que otros developers detecten errores
+//al usar nuestros componentes.
+//Con isRequired nos obliga a ser utilizado.
+FirstApp.propTypes = {
+  title:PropTypes.string.isRequired,
+  subTitle:PropTypes.number.isRequired,
 }
