447f613d474fdc8ec03c9d7e93db696888ca57b6
Jorge Bugarini <l21550317@chihuahua2.tecnm.mx>
2024-07-15 15:02:52 -0500

    Instalacion de types de Jest y leccion de Expect-toBe

--- 03-counter-app-vite/tests/demo.test.js
+++ 03-counter-app-vite/tests/demo.test.js
@@ -1,7 +1,28 @@
+describe('Pruebas en <DemoComponent>', () => {
+
 test('Esta prueba no deberia fallar', () =>{
 
 
-    if(1===0){
-        throw new Error('No puede dividir entre 0');
-    }
-})
+    // Las pruebas se caracterizan por tener el siguiente cuerpo
+
+    // 1. Inicialización
+    // 2. Estímulo
+    // 3. Observacion del comportamiento esperado
+
+    // En la primera prueba los mensajes deben ser identicos
+    const message1 =  'Hello World';
+
+    const message2 = message1.trim();
+
+
+    // ToBe es un metodo que compara si dos valores son iguales
+    // Y pasa la prueba si son iguales
+    expect(message1).toBe(message2);
+
+
+    // La instalacion de @types/jest nos ayuda a identificar de mejor
+    // manera las pruebas de JEST que podemos utilizar pues
+    //de esta forma los metodos explican que son
+
+    })
+});
