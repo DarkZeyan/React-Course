032da1171e25bc36f3a9645e61cba515d0385dfb
Jorge Bugarini <l21550317@chihuahua2.tecnm.mx>
2024-09-03 12:44:10 -0600

    Pruebas con async-await

---
+++ 03-counter-app-vite/tests/base-pruebas/11-async-await.test.js
@@ -0,0 +1,16 @@
+import { getImagen } from "../../src/base-pruebas/11-async-await";
+
+describe(
+    'Pruebas en 11-async-await.js', () => {
+
+
+        test('getImagen debe retornar el url de la imagen', async() => {
+        
+            const url = await getImagen();
+            console.log(url);
+            expect(typeof url).toBe('string');
+        });
+
+
+    }
+)
